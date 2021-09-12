e958cc417068d91aadb266d13f1b5085fa4e2b26
LeonVieira <leonVieira456@gmail.com>
2021-09-12 11:00:13 +0200

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BizzWheelSummartive_2.Startup))]
+namespace BizzWheelSummartive_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
